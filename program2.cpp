04e4c6fd507ab679386794b386566fb1cbbf9edd
cummingska3 <46767766+cummingska3@users.noreply.github.com>
2019-01-22 18:36:01 -0500

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,19 @@
+// Program #2 - Using a variable
+
+
+#include <pch.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x; //this declares variable
+	x = 1023; // this assigns 1023 to x
+
+	cout << "This program prints the value of x; ";
+	cout << x; //This displays 1023
+
+	return 0;
+}
+
+
